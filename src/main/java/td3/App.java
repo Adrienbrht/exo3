d837e4848f34f63d1a28b7d865625d937cfcec91
adrienbrht <adrien.brohet@ltpdampierre.fr>
2022-10-11 10:48:18 +0200

    first commit

---
+++ src/main/java/td3/App.java
@@ -0,0 +1,26 @@
+package td3;
+
+import java.util.Random;
+
+public class App {
+    
+    public static void main(String[] args) {
+
+        int total = 0;
+        int nbrEssais = 5;
+
+        Random generateur = new Random();
+
+        for(int i = 0; i < nbrEssais; i++) {
+
+            int lancer = (generateur.nextInt(6) + 1);
+
+            total += lancer;
+
+        }
+        System.out.println(total == 20 ? "Vous avez gagné !" : "Perdu ! ");
+
+    }
+    
+}
+    
