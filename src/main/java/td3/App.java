90c11c6859f89f2f8dcb3d28c617027f51f960c7
adrienbrht <adrien.brohet@ltpdampierre.fr>
2022-10-11 11:36:59 +0200

    td4 loie

--- src/main/java/td3/App.java
+++ src/main/java/td3/App.java
@@ -3,24 +3,29 @@ package td3;
 import java.util.Random;
 
 public class App {
-    
+
     public static void main(String[] args) {
 
         int total = 0;
         int nbrEssais = 5;
+        while (total != 20) {
 
-        Random generateur = new Random();
+            total = 0;
+            Random generateur = new Random();
 
-        for(int i = 0; i < nbrEssais; i++) {
+            for (int i = 0; i < nbrEssais; i++) {
 
-            int lancer = (generateur.nextInt(6) + 1);
+                int lancer = (generateur.nextInt(6) + 1);
+                System.out.println("Vous avez fait :" + lancer);
 
-            total += lancer;
+                total += lancer;
+                System.out.println("Vous etes a la case :" + total);
 
+            }
+            System.out.println("Perdu ! ");
         }
-        System.out.println(total == 20 ? "Vous avez gagné !" : "Perdu ! ");
+        System.out.println("vous avez gagné ! ");
 
     }
-    
+
 }
-    
